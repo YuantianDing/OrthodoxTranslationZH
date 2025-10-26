95eb78bd7effa88c9c7d5f5e5f71ddb9d9c17410
Yuantian Ding <work.yt.ding@gmail.com>
2025-10-26 16:00:06 -0400

    Fix webiste footnote.

--- website/components/orthodox-comparison.tsx
+++ website/components/orthodox-comparison.tsx
@@ -52,7 +52,7 @@ function parseTextWithFootnotes(
   }
   return regexReplaceAll(text, /\[([авaв]?\d+)\]/g, match => {
     const footnoteId = match[1].replace("a", "а").replace("в", "в")
-    const footnote = footnotes[match[0]]; 
+    const footnote = footnotes[footnoteId]; 
     if (footnote) {
       return <FootnoteMarker key={`${footnoteId}-${match.index}`} id={footnoteId} footnote={footnote} language={language} />;
     }
